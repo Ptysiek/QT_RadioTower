4c619a8ab59ade416c6f97745429c625b9db73b2
Kacu <k.ptychu@gmail.com>
2021-04-05 00:05:56 +0200

    project cleanup

--- RadioTower.h
+++ RadioTower.h
@@ -1,6 +1,4 @@
 #pragma once
-
-#include <memory>
 #include <QObject>
 #include <vector>
 
--- World.h
+++ World.h
@@ -1,6 +1,4 @@
 #pragma once
-
-#include <exception>
 #include <memory>
 #include <QRandomGenerator>
 #include <vector>
